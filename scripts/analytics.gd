0a3b800f4f82d683d2502454694c92f63f263d38
Spaceship1842 <m8826706@gmail.com>
2024-03-28 14:58:40 +0800

    Save mechanic start

---
+++ scripts/analytics.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name Analytics
+
+@export var best_score: int
+@export var games_played: int
+@export var lasers_dodged: int
+@export var best_lasers_dodged: int
+@export var all_tisten_bolts_survived: int
+@export var best_tisten_bolts_survived: int
---
+++ scripts/save_game.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name GameSave
+
+@export var first_play: bool
+@export var last_score: int
+@export var best_score: int
+@export var games_played: int
+@export var lasers_dodged: int
+@export var best_lasers_dodged: int
+@export var all_tisten_bolts_survived: int
+@export var best_tisten_bolts_survived: int
