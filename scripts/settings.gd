290665fe71413984fc4e4c9db2afb7c4d4928054
Spaceship1842 <m8826706@gmail.com>
2024-04-26 13:46:26 +0800

    did it

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -5,6 +5,13 @@ extends Window
 signal reset
 signal changelog_show
 
+func _process(delta):
+	if $TabBar.current_tab == 0: # The player tab
+		$V2.visible = true
+		$V.visible = false
+	if $TabBar.current_tab == 1: # The misc tab
+		$V2.visible = false
+		$V.visible = true
 
 func _on_reset_save_pressed():
 	emit_signal("reset")
@@ -12,3 +19,7 @@ func _on_reset_save_pressed():
 
 func _on_changelog_pressed():
 	emit_signal("changelog_show")
+
+
+func _on_tab_bar_tab_clicked(tab):
+	$TabBar.current_tab = tab
